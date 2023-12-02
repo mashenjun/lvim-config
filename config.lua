caea86685976d594cd5cc23a68352e51529d87fa
Shenjun <mashenjun0902@gmail.com>
2023-12-03 01:22:30 +0800

    add some plugin to enhance quickfix window and symbols outline
    Signed-off-by: Shenjun <mashenjun0902@gmail.com>

--- config.lua
+++ config.lua
@@ -273,6 +273,68 @@ lvim.plugins = {
   { "fatih/vim-go",
     ft = { "go" }
   },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() 
+      require"lsp_signature".on_attach({
+        hint_enable = true,
+        floating_window = false,
+      }) 
+    end,
+  },
+  {
+    "ahmedkhalf/lsp-rooter.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp-rooter").setup()
+    end,
+  },
+  {
+    "rmagatti/goto-preview",
+    config = function()
+      require('goto-preview').setup({
+        width = 120; -- Width of the floating window
+        height = 25; -- Height of the floating window
+        default_mappings = true; -- Bind default mappings
+        debug = false; -- Print debug information
+        opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
+        -- You can use "default_mappings = true" setup option
+        -- Or explicitly set keybindings
+        -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+        -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+        -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+      })
+    end
+  },
+  {
+    "kevinhwang91/nvim-bqf",
+    event = { "BufRead", "BufNew" },
+    config = function()
+      require("bqf").setup({
+        auto_enable = true,
+        auto_resize_height = true,
+        preview = {
+          win_height = 20,
+          win_vheight = 15,
+          delay_syntax = 80,
+          border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+        },
+        func_map = {
+          vsplit = "",
+          ptogglemode = "z,",
+          stoggleup = "",
+        },
+        filter = {
+          fzf = {
+            action_for = { ["ctrl-s"] = "split" },
+            extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+          },
+        },
+      })
+    end,
+  },
   --     {
   --       "folke/trouble.nvim",
   --       cmd = "TroubleToggle",
